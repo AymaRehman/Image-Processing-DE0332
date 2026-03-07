0534e6dbc6a1bbb489090989849bf7fe253620f9
AymaRehman <ayma.rehman24@gmail.com>
2026-03-07 22:13:39 +0200

    defining main correcting functions to be filled in later

---
+++ Lab2/src/contrast_correction.py
@@ -0,0 +1,17 @@
+# Ayma Rehman
+# 241ADB165
+# Lab 2
+
+import numpy as np
+
+
+def gamma_correction(img, gamma=1.8):
+    pass
+
+
+def histogram_linear_correction(img):
+    pass
+
+
+def compute_histogram_fast(channel):
+    pass
