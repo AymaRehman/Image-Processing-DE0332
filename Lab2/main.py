643b6c3d5b91a656bae4e8eba6b28faf34184b63
AymaRehman <ayma.rehman24@gmail.com>
2026-03-08 21:17:14 +0200

    initializing imports for main.py

---
+++ Lab2/main.py
@@ -0,0 +1,13 @@
+# Ayma Rehman
+# 241ADB165
+# Lab 2
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import os
+from src.contrast_correction import (
+    gamma_correction,
+    histogram_linear_correction,
+    compute_histogram_fast,
+)
